1e2c4721163975d36d0ce05389a869b86382fd77
Saki2502 <tuyetnhi1610@gmail.com>
2022-02-11 18:16:40 +0700

    update home + gallery

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
-  $(".sub-menu").hover(function () {
-    $("ul").show();
+  $("#go-to-top").hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 100) $("#go-to-top").show("slow");
+    else $("#go-to-top").hide("slow");
   });
-  
+  $("#go-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500);
+  });
+
+  //Automatic Slideshow
+  var myIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+      myIndex = 1;
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+  }
 });
