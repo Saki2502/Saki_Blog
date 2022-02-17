df7adb3c95e01489b1d874c10a09ea93e6a2b266
Nguyễn Đặng Tuyết Nhi <87234760+Saki2502@users.noreply.github.com>
2022-02-17 21:57:07 +0700

    update

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
-$(document).ready(function () {
-  $("#go-to-top").hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() >= 100) $("#go-to-top").show("slow");
-    else $("#go-to-top").hide("slow");
-  });
-  $("#go-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1500);
-  });
+$(document).ready(function() {
+    $("#header, #footer").addClass('wow animate__heartBeat')
+    $("#person-img > img").addClass('wow animate__tada')
+    $("#container > div.content-section").addClass('wow animate__backInLeft')
+    $("#container > div.content-section1").addClass('wow animate__backInRight')
 
-  //Automatic Slideshow
-  var myIndex = 0;
-  showSlides();
+    wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animate__animated',
+        offset: 0,
+        mobile: true,
+        live: true
+    })
 
-  function showSlides() {
-    var i;
-    var x = document.getElementsByClassName("slides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+    wow.init();
+
+    $("#go-to-top").hide();
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 100) $("#go-to-top").show("slow");
+        else $("#go-to-top").hide("slow");
+    });
+    $("#go-to-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 1500);
+    });
+
+    //Automatic Slideshow
+    var myIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var x = document.getElementsByClassName("slides");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1;
+        }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(showSlides, 2000);
     }
-    myIndex++;
-    if (myIndex > x.length) {
-      myIndex = 1;
-    }
-    x[myIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000);
-  }
 });
